de57725be7edf948bcca4f31fdbc8e3c0e144b0e
maihnguyen0221 <maihnguyen0221@gmail.com>
2025-04-06 23:19:28 -0600

    fixed sign up

--- server.js
+++ server.js
@@ -42,27 +42,30 @@ function getJSONObjectForMovieRequirement(req) {
     return json;
 }
 
-router.post('/signup', function(req, res) {
+router.post('/signup', async (req, res) => { // Use async/await
     if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please include both username and password to signup.'})
-    } else {
-        var user = new User();
-        user.name = req.body.name;
-        user.username = req.body.username;
-        user.password = req.body.password;
-
-        user.save(function(err){
-            if (err) {
-                if (err.code == 11000)
-                    return res.json({ success: false, message: 'A user with that username already exists.'});
-                else
-                    return res.json(err);
-            }
-
-            res.json({success: true, msg: 'Successfully created new user.'})
-        });
+      return res.status(400).json({ success: false, msg: 'Please include both username and password to signup.' }); // 400 Bad Request
     }
-});
+  
+    try {
+      const user = new User({ // Create user directly with the data
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password,
+      });
+  
+      await user.save(); // Use await with user.save()
+  
+      res.status(201).json({ success: true, msg: 'Successfully created new user.' }); // 201 Created
+    } catch (err) {
+      if (err.code === 11000) { // Strict equality check (===)
+        return res.status(409).json({ success: false, message: 'A user with that username already exists.' }); // 409 Conflict
+      } else {
+        console.error(err); // Log the error for debugging
+        return res.status(500).json({ success: false, message: 'Something went wrong. Please try again later.' }); // 500 Internal Server Error
+      }
+    }
+  });
 
 router.post('/signin', async (req, res) => { // Use async/await
     try {
