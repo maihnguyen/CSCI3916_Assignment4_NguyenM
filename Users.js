e1c146989fb8a58971d288aab527d3bb7b4e4c43
maihnguyen0221 <maihnguyen0221@gmail.com>
2025-04-06 23:30:01 -0600

    revert files

--- Users.js
+++ Users.js
@@ -1,49 +1,47 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt'); // Use bcrypt, not bcrypt-nodejs
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var bcrypt = require('bcrypt-nodejs');
 
-// No need for mongoose.Promise = global.Promise;
-require('dotenv').config();
+mongoose.Promise = global.Promise;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB); // useNewUrlParser and useUnifiedTopology are no longer needed
-    console.log("Connected to MongoDB");
-  } catch (error) {
-    console.error("MongoDB connection error:", error); // Log the actual error object
-    process.exit(1); // Exit the process if the connection fails (optional, but good practice)
-  }
-};
+//mongoose.connect(process.env.DB, { useNewUrlParser: true });
+try {
+    mongoose.connect( process.env.DB, {useNewUrlParser: true, useUnifiedTopology: true}, () =>
+        console.log("connected"));
+}catch (error) {
+    console.log("could not connect");
+}
+mongoose.set('useCreateIndex', true);
 
-connectDB();
-
-
-const UserSchema = new Schema({
+//user schema
+var UserSchema = new Schema({
     name: String,
-    username: { type: String, required: true, index: { unique: true } },
+    username: { type: String, required: true, index: { unique: true }},
     password: { type: String, required: true, select: false }
 });
 
-UserSchema.pre('save', async function(next) {  // Use async/await for cleaner code
-    const user = this;
+UserSchema.pre('save', function(next) {
+    var user = this;
 
+    //hash the password
     if (!user.isModified('password')) return next();
 
-    try {
-        const hash = await bcrypt.hash(user.password, 10); // 10 is the salt rounds (adjust as needed)
+    bcrypt.hash(user.password, null, null, function(err, hash) {
+        if (err) return next(err);
+
+        //change the password
         user.password = hash;
         next();
-    } catch (err) {
-        return next(err);
-    }
+    });
 });
 
-UserSchema.methods.comparePassword = async function(password) { // Use async/await
-    try {
-        return await bcrypt.compare(password, this.password);
-    } catch (err) {
-        return false; // Or handle the error as you see fit
-    }
-};
+UserSchema.methods.comparePassword = function (password, callback) {
+    var user = this;
 
+    bcrypt.compare(password, user.password, function(err, isMatch) {
+        callback(isMatch);
+    })
+}
+
+//return the model to server
 module.exports = mongoose.model('User', UserSchema);
